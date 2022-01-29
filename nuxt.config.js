e39f36475303765104e11f75472bc2e42ca1db09
Pelayo Suárez <pesuafer@gmail.com>
2022-01-29 20:12:42 +0100

    feat: added new page, added modules to improve app styling

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,11 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    'normalize.css',
+    '~assets/scss/main.scss',
+    '~assets/scss/colors.scss'
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -35,8 +39,14 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+  modules: [
+    '@nuxtjs/style-resources'
+  ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
+
+  styleResources: {
+    scss: ['./assets/scss/*.scss']
+  },
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,21 @@
 <template>
-  <Tutorial />
+  <div id="wrapper">
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'IndexPage',
+  name: 'TaskPage',
 })
 </script>
+
+<style lang="scss" scoped>
+  #wrapper {
+    position: relative;
+    height: 100%;
+    width: 100%;
+    background-color: $c_gray;
+  }
+</style>
